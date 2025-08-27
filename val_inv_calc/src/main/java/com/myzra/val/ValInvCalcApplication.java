c5e1a36ec275f2c2d135a317048d686ed25d9aba
myzra <myzrahfortnite@gmail.com>
2025-08-28 00:50:57 +0200

    added supabase db connection credentials

--- val_inv_calc/src/main/java/com/myzra/val/ValInvCalcApplication.java
+++ val_inv_calc/src/main/java/com/myzra/val/ValInvCalcApplication.java
@@ -2,12 +2,16 @@ package com.myzra.val;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ValInvCalcApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
 		SpringApplication.run(ValInvCalcApplication.class, args);
 	}
-
 }
