0a051bce66a33f8dd519895002821f4515dd32a0
myzra <myzrahfortnite@gmail.com>
2025-08-27 23:20:53 +0200

    init springboot backend

---
+++ val_inv_calc/src/test/java/com/myzra/val/ValInvCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myzra.val;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValInvCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
