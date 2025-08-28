1bb0c3827a285018a75e9812e45106d7c70e4419
myzra <myzrahfortnite@gmail.com>
2025-08-28 20:26:47 +0200

    added for each loop to load app properties

--- val_inv_calc/src/main/java/com/myzra/val/ValInvCalcApplication.java
+++ val_inv_calc/src/main/java/com/myzra/val/ValInvCalcApplication.java
@@ -8,10 +8,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ValInvCalcApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
 		SpringApplication.run(ValInvCalcApplication.class, args);
 	}
 }
