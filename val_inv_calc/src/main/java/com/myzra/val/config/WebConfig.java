c870b0be83ede282e073b893f285a8c1f919590e
myzra <myzrahfortnite@gmail.com>
2025-08-30 22:56:53 +0200

    updated CORS settings

--- val_inv_calc/src/main/java/com/myzra/val/config/WebConfig.java
+++ val_inv_calc/src/main/java/com/myzra/val/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5500") // Add your frontend URLs
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5500", "http://127.0.0.1:5500")
+                .allowedMethods("GET")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
