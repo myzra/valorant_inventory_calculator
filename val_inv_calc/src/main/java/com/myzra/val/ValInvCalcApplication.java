3f987a7e14c94f49cbecb859c08db71f6be114cc
myzra <myzrahfortnite@gmail.com>
2025-08-31 14:03:46 +0200

    changed so dotenv is only being used in a local environment

--- val_inv_calc/src/main/java/com/myzra/val/ValInvCalcApplication.java
+++ val_inv_calc/src/main/java/com/myzra/val/ValInvCalcApplication.java
@@ -6,12 +6,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ValInvCalcApplication {
-
-	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        dotenv.entries().forEach(entry ->
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
-		SpringApplication.run(ValInvCalcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        if (System.getenv("RENDER") == null) { // Render sets this automatically
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+            );
+        }
+        SpringApplication.run(ValInvCalcApplication.class, args);
+    }
 }
